22efd5f39eb85d967ea5770bc81e34b804292e64
Dominic Burnard <dominic.burnard@gmail.com>
2020-12-02 22:48:59 +0000

    Update to work out the combinations using a seperate routine in Day 1

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -11,15 +11,15 @@ namespace AdventOfCode.Solutions.Year2020
 
         public Day01() : base(01, 2020, "")
         {
-//            DebugInput = @"1721
-//979
-//366
-//299
-//675
-//1456";
+            //DebugInput = @"1721
+            //979
+            //366
+            //299
+            //675
+            //1456";
         }
 
-        protected override string SolvePartOne()
+        protected string SolvePartOneOld()
         {
             var values = Input.SplitByNewline().Select(x => int.Parse(x)).ToList();
             var length = values.Count;
@@ -39,7 +39,23 @@ namespace AdventOfCode.Solutions.Year2020
             throw new InvalidOperationException();
         }
 
-        protected override string SolvePartTwo()
+
+        protected override string SolvePartOne()
+        {
+            var values = Input.SplitByNewline().Select(x => int.Parse(x)).ToArray();
+
+            foreach (var item in GetCombinations(values, 2))
+            {
+                if (item[0] + item[1] == 2020)
+                {
+                    var result = (item[0] * item[1]);
+                    return result.ToString();
+                }
+            }
+            throw new InvalidOperationException();
+        }
+
+        protected string SolvePartTwoOld()
         {
             var values = Input.SplitByNewline().Select(x => int.Parse(x)).ToList();
             var length = values.Count;
@@ -59,6 +75,72 @@ namespace AdventOfCode.Solutions.Year2020
                 }
             }
             throw new InvalidOperationException();
+        }
+
+        protected override string SolvePartTwo()
+        {
+            var values = Input.SplitByNewline().Select(x => int.Parse(x)).ToArray();
+
+            foreach (var item in GetCombinations(values, 3))
+            {
+                if (item[0] + item[1] + item[2] == 2020)
+                {
+                    var result = (item[0] * item[1] * item[2]);
+                    return result.ToString();
+                }
+            }
+            throw new InvalidOperationException();
+        }
+
+
+        protected IEnumerable<T[]> GetCombinations<T>(T[] items, int count)
+        {
+            int[] index = new int[count];
+            int[] maxIndex = new int[count];
+
+            for (int initial = 0; initial < count; initial++)
+            {
+                index[initial] = initial;
+                maxIndex[initial] = items.Length - count + initial;
+            }
+
+            var carryOn = true;
+            while (carryOn)
+            {
+                yield return outputCurrentIndexes();
+                carryOn = IncrementIndexes();
+            }
+
+            yield break;
+
+            bool IncrementIndexes()
+            {
+                for (int checkIndex = count - 1; checkIndex >= 0; checkIndex--)
+                {
+                    if (index[checkIndex] < maxIndex[checkIndex])
+                    {
+                        index[checkIndex]++;
+                        for (int resetIndexOffset = 1; resetIndexOffset < count - checkIndex; resetIndexOffset++)
+                        {
+                            index[checkIndex + resetIndexOffset] = index[checkIndex] + resetIndexOffset;
+                        }  
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            T[] outputCurrentIndexes()
+            {
+                var values = new T[count];
+                for (int current = 0; current < count; current++)
+                {
+                    values[current] = items[index[current]];
+                }
+                return values;
+            }
+
         }
     }
 }
