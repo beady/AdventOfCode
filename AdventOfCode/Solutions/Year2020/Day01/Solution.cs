5935c75e04220f052c9ca86aff4e15c6a7d4e232
Dominic Burnard <dominic.burnard@gmail.com>
2020-12-02 21:19:36 +0000

    Solutions for day 1 + 2

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -10,17 +11,54 @@ namespace AdventOfCode.Solutions.Year2020
 
         public Day01() : base(01, 2020, "")
         {
-
+//            DebugInput = @"1721
+//979
+//366
+//299
+//675
+//1456";
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var values = Input.SplitByNewline().Select(x => int.Parse(x)).ToList();
+            var length = values.Count;
+
+            for (int index1 = 0; index1 < values.Count; index1++)
+            {
+                for (int index2 = index1 + 1; index2 < values.Count; index2++)
+                {
+                    if (values[index1] + values[index2] == 2020)
+                    {
+                        var result = (values[index1] * values[index2]);
+                        return (values[index1] * values[index2]).ToString();
+                    }
+                }
+            }
+
+            throw new InvalidOperationException();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var values = Input.SplitByNewline().Select(x => int.Parse(x)).ToList();
+            var length = values.Count;
+
+            for (int index1 = 0; index1 < values.Count; index1++)
+            {
+                for (int index2 = index1 + 1; index2 < values.Count; index2++)
+                {
+                    for (int index3 = index2 + 1; index3 < values.Count; index3++)
+                    {
+                        if (values[index1] + values[index2] + values[index3] == 2020)
+                        {
+                            var result = (values[index1] * values[index2] * values[index3]);
+                            return result.ToString();
+                        }
+                    }
+                }
+            }
+            throw new InvalidOperationException();
         }
     }
 }
--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -10,17 +11,58 @@ namespace AdventOfCode.Solutions.Year2020
 
         public Day02() : base(02, 2020, "")
         {
-
+//            DebugInput = @"1-3 a: abcde
+//1-3 b: cdefg
+//2-9 c: ccccccccc";
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var values = Input.SplitByNewline();
+
+            var validCount = 0;
+
+            foreach (var item in values)
+            {
+                var parts = item.Split(":");
+                var policyParts = parts[0].Split(" ");
+                var counts = policyParts[0].Split("-");
+                var policy = new PasswordPolicy(int.Parse(counts[0]), int.Parse(counts[1]), policyParts[1].FirstOrDefault());
+
+                var validPasswordChars = parts[1].Where(x => x.Equals(policy.letter)).ToArray();
+                if (validPasswordChars.Length >= policy.Min && validPasswordChars.Length <= policy.Max)
+                {
+                    validCount++;
+                }
+            }
+
+            return validCount.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var values = Input.SplitByNewline();
+
+            var validCount = 0;
+
+            foreach (var item in values)
+            {
+                var parts = item.Split(":");
+                var policyParts = parts[0].Split(" ");
+                var counts = policyParts[0].Split("-");
+                var policy = new PasswordPolicy(int.Parse(counts[0]), int.Parse(counts[1]), policyParts[1].FirstOrDefault());
+
+                var password = parts[1];
+
+                if ((password[policy.Min] == policy.letter) ^ (password[policy.Max] == policy.letter))
+                {
+                    validCount++;
+                }
+            }
+
+            return validCount.ToString();
         }
+
+        private record PasswordPolicy(int Min, int Max, char letter) { };
     }
 }
